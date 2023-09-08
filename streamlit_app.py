c5597ccac2884eb4ce207092662779c7e99304b5
shamilbava <143556243+shamilbava@users.noreply.github.com>
2023-09-08 12:35:53 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,14 @@
+#App Import
 import streamlit
-
+#Add title
 streamlit.title('My Parents New Healthy Diner')
-
+#Add headers
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach, & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
-
+#Add picklist header
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
@@ -19,9 +20,9 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
-
+#Display FruityVice API repsonse
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
 
 streamlit.header("Fruityvice Fruit Advice!")
